506246132f839b5ed051767fef479ad6e4c94cbd
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2013-05-24 16:21:39 -0400

    Added keywords, description, license, and changed URL to github.

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from distutils.core import setup
 
-# Good instructions for adding a package to PyPi
-#  https://jamiecurle.co.uk/blog/my-first-experience-adding-package-pypi/
-
-
 with open('README.rst', 'r') as fh:  txt = fh.read()
 with open('LICENSE.txt', 'r') as fh:  lic = fh.read()
 script = "s = '''\n" + txt + '\n' + lic + "\n'''\n\ndef readme(): return s"
@@ -11,9 +7,12 @@ with open('elephants/readme.py', 'w') as fh: fh.write(script)
 
 setup(
   name='Elephants',
-  version='0.1.0',
+  version='0.1.1',
   author='Marc Schwarzschild',
   author_email='ms@TheBrookhavenGroup.com',
+  url='http://github.com/Schwarzschild/Elephants',
+  license='MIT',
+  description='Memoization utilities with fading memory.',
+  keywords=['memo', 'memoize', 'cache'],
   packages=['elephants',],
-  url='http://pypi.python.org/pypi/Elephants/',
 )
